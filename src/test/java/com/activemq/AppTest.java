ed57fcb698c764f232f2548c96707905175278e0
lihonghui <lihonghui@xlpayment.com>
2019-11-28 16:50:29 +0800

    生产者

--- src/test/java/com/activemq/AppTest.java
+++ src/test/java/com/activemq/AppTest.java
@@ -6,6 +6,7 @@ import javax.jms.Message;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,24 +30,27 @@ public class AppTest {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		// 第二步：从容器中获得JMSTemplate对象。
 		long starttime = System.currentTimeMillis();
-		for (int i = 0; i < 10000; i++) {
+		for (int i = 1; i <=1000; i++) {
+			final String sendmessage = String.valueOf(i);
 			JmsTemplate jmsTemplate = (JmsTemplate) applicationContext.getBean("jmsTemplate");
 			// 第三步：从容器中获得一个Destination对象
-			Queue queue = (Queue) applicationContext.getBean("txnQueue");
+//			Queue queue = (Queue) applicationContext.getBean("txnQueue");
+			Topic queue = (Topic) applicationContext.getBean("txnTopic");
 			// 第四步：使用JMSTemplate对象发送消息，需要知道Destination
 			jmsTemplate.send(queue, new MessageCreator() {
 				
 				@Override
 				public Message createMessage(Session session) throws JMSException {
-					TextMessage textMessage = session.createTextMessage("hello activmq this is message");
+					TextMessage textMessage = session.createTextMessage("第"+sendmessage+"条消息:hello consumer I am producer");
 					return textMessage;
 				}
 			});
 		}
 				
 		long endtime = System.currentTimeMillis();
-		System.out.println("总耗时:"+(endtime-starttime)+"ms");
+		System.out.println("发送总耗时:"+(endtime-starttime)+"ms");
 
 	}
+	
 	
 }
